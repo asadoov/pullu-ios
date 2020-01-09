66b600f94fd503f52d6c598824ee33a3fd6516d4
Rufat Asadzade <asadzade99@gmail.com>
2020-01-09 15:43:16 +0400

    appDelegate fixed

--- pullu.test/AppDelegate.swift
+++ pullu.test/AppDelegate.swift
@@ -12,13 +12,13 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+ var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         
+           return true
         
-        return true
     }
 
     // MARK: - Core Data stack
