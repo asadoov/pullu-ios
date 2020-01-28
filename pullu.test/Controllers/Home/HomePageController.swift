82cc08a74aae3114e5191b4329956a74b15eef84
javidanmirza <jmirzabejli@gmail.com>
2020-01-28 13:30:57 +0400

    works

--- pullu.test/Controllers/Home/HomePageController.swift
+++ pullu.test/Controllers/Home/HomePageController.swift
@@ -195,7 +195,7 @@ class HomePageController: UIViewController,UITableViewDelegate,UITableViewDataSo
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell: ReklamCellTableViewCell = (tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ReklamCellTableViewCell)
-        cell.object = dataArray[indexPath.row]
+	        cell.object = dataArray[indexPath.row]
         advertID=cell.object?.id!
         print(advertID!)
         self.performSegue(withIdentifier: "aboutAdvertPage", sender: self)
