5866eabcc8e3941654c26c9a11fce31602ee2987
Rufat Asadzade <32931813+asadoov@users.noreply.github.com>
2020-02-14 20:07:37 +0400

    save

---
+++ pullu.test/Controllers/ProfileController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileController.swift
+//  pullu.test
+//
+//  Created by Rufat on 2/13/20.
+//  Copyright © 2020 Rufat Asadzade. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    @IBOutlet weak var saveBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        let gradient = CAGradientLayer()
+
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.black.cgColor]
+
+        saveBtn.layer.insertSublayer(gradient, at: 0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
