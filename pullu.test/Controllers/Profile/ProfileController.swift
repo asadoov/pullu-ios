1950f296e410e904c6e67910f03d510cf7a49565
Rufat Asadzade <32931813+asadoov@users.noreply.github.com>
2020-04-30 01:49:23 +0400

    save

--- pullu.test/Controllers/Profile/ProfileController.swift
+++ pullu.test/Controllers/Profile/ProfileController.swift
@@ -10,7 +10,51 @@ import UIKit
 
 //Cavidan Mirzə
 
-class ProfileController: UIViewController {
+class ProfileController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var genderList = ["Kişi","Qadın"]
+    var countriesList = ["Ölkəni seçin"]
+       var cityList = ["Şəhəri seçin"]
+       var professionList = ["Sektoru seçin"]
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+var countrows : Int=genderList.count
+        
+//if pickerView == countries {
+//    countrows = self.countriesList.count
+//}
+//if pickerView == cities {
+//    countrows = self.cityList.count
+//}
+//if pickerView == professions {
+//    countrows = self.professionList.count
+//}
+
+return countrows
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+         return genderList[row]
+//        if pickerView == countries {
+//
+//            return countriesList[row]
+//        }
+//        if pickerView == cities {
+//
+//            return cityList[row]
+//        }
+//        if pickerView == professions {
+//
+//            return professionList[row]
+//        }
+        return ""
+        
+    }
+    
     
     
     @IBOutlet weak var surnameField: UITextField!
@@ -18,41 +62,52 @@ class ProfileController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var mobileNumField: UITextField!
     @IBOutlet weak var dogumTarixField: UITextField!
-    @IBOutlet weak var creatDate: UITextField!
-    @IBOutlet weak var genderField: UITextField!
-    @IBOutlet weak var ixtisas: UITextField!
-    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var createdDate: UITextField!
     
+    @IBOutlet weak var genderButton: UIButton!
+    
+    @IBOutlet weak var professionButton: UIButton!
+    
+    @IBOutlet weak var countryButton: UIButton!
+    
+    @IBOutlet weak var cityButton: UIButton!
     
     
     
     var defaults = UserDefaults.standard
     var select:dbSelect=dbSelect()
-    var profilM: [ProfileModel] = [ProfileModel]()
-    
+    var profileList: [ProfileModel] = [ProfileModel]()
+     var uProfile = UpdateProfileStruct()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = Locale.current
         
         let pass = defaults.string(forKey: "pass")
         let mail = defaults.string(forKey: "mail")
         select.getProfileInfo(mail: mail, pass: pass) {
             (list) in
-   
+            self.profileList = list
             DispatchQueue.main.async {
                 self.emailField.text = list[0].mail
                 self.nameField.text = list[0].name
                 self.surnameField.text = list[0].surname
                 self.mobileNumField.text = list[0].phone
-                self.dogumTarixField.text = list[0].bDate
-                self.creatDate.text = list[0].cDate
-                self.genderField.text = list[0].gender
-                self.ixtisas.text = list[0].profession
-                self.cityField.text = list[0].city
-              
+                self.genderButton.setTitle(list[0].gender, for: .normal)
+                  self.countryButton.setTitle(list[0].country, for: .normal)
+            self.cityButton.setTitle(list[0].city, for: .normal)
+           
+                
+                var bFormattedDate = dateFormatter.date(from: list[0].bDate!)
+                var createdFormattedDate = dateFormatter.date(from: list[0].cDate!)
+                dateFormatter.dateFormat = "dd.MM.yyyy"
+                self.dogumTarixField.text = dateFormatter.string(from:bFormattedDate!)
+                self.createdDate.text = dateFormatter.string(from:createdFormattedDate!)
+                
             }
             
         }
@@ -70,6 +125,58 @@ class ProfileController: UIViewController {
         super.viewWillDisappear(animated)
         
     }
+    @IBAction func genderButtonClick(_ sender: Any) {
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: 250,height: 300)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 250, height: 300))
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        vc.view.addSubview(pickerView)
+        let editRadiusAlert = UIAlertController(title: "Cinsi seçin...", message: "", preferredStyle: UIAlertController.Style.alert)
+        editRadiusAlert.setValue(vc, forKey: "contentViewController")
+        editRadiusAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
+        editRadiusAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(editRadiusAlert, animated: true)
+//        let myDatePicker: UIDatePicker = UIDatePicker()
+//        myDatePicker.timeZone = .current
+//          myDatePicker.frame = CGRect(x: 0, y: 15, width: 270, height: 200)
+//          let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .alert)
+//          alertController.view.addSubview(myDatePicker)
+//          let selectAction = UIAlertAction(title: "Ok", style: .default, handler: { _ in
+//              print("Selected Date: \(myDatePicker.date)")
+//          })
+//          let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+//          alertController.addAction(selectAction)
+//          alertController.addAction(cancelAction)
+//          present(alertController, animated: true)
+    }
+    
+    @IBAction func professionClick(_ sender: Any) {
+    }
+    @IBAction func saveButtonClicked(_ sender: Any) {
+   
+        
+        if(nameField.text != profileList[0].name){
+            uProfile.name = nameField.text
+            
+        }
+        if(surnameField.text != profileList[0].name){
+            uProfile.surname = surnameField.text
+            
+        }
+        if(mobileNumField.text != profileList[0].phone){
+            uProfile.phone = mobileNumField.text
+            
+        }
+        if(emailField.text != profileList[0].mail){
+                 uProfile.mail = emailField.text
+                 
+             }
+     
+        
+    }
+    
+    
     
     /*
      // MARK: - Navigation
---
+++ pullu.test/Models/Request/UpdateProfileStruct.swift
@@ -0,0 +1,22 @@
+//
+//  UpdateProfileStruct.swift
+//  pullu.test
+//
+//  Created by Rufat on 4/30/20.
+//  Copyright © 2020 Rufat Asadzade. All rights reserved.
+//
+
+import Foundation
+class UpdateProfileStruct: Codable {
+    var name: String?
+    var surname: String?
+    var mail: String?
+    var pass: String?
+    var phone: String?
+    var birthDate: String?
+    var genderID: Int?
+    var countryID: Int?
+    var cityID: Int?
+    var professionID: Int?
+    
+}
